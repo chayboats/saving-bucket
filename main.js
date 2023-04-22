b05ffe59bda811028dae8cbcef2a1faf01ef7daf
Chaley Boatmon <chaleyboatmon@gmail.com>
2023-04-21 21:01:52 -0400

    Update saving bucket

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const cardForm = document.getElementById('card-form');
 const cardContainer = document.getElementById('cards');
-
-const typeSelect = document.getElementById('type-select');
 const amountInput = document.getElementById('amount-input');
-
+const typeSelect = document.getElementById('type-select');
 const typeOptions = typeSelect.children
+const optionNumber = typeSelect.value
 
-function createElement(elementType, options = {}) {
+function createElement(elementType, options = {}, parent = "") {
   const element = document.createElement(elementType);
   const optionKeys = Object.keys(options)
 
@@ -18,28 +17,29 @@ function createElement(elementType, options = {}) {
     element[key].add(...options[key]);  
   })
 
+  if(parent !="" ) {
+    parent.append(element);
+  }
+
   return element
 }
 
-function children(parent) {
-  return parent.children
-}
-
-function createIcon(index) {
-  const iconOptions = ['fa-burger', 'fa-file-invoice-dollar', 'fa-house', 'fa-suitcase-medical', 'fa-bus', 'fa-film', 'fa-layer-group'];
-  createElement('i', { classList: ['icon', 'fa-solid', iconOptions[index]] })
+function createIcon(array, index, parent) {
+  return createElement('i', { classList: ['icon', 'fa-solid', array[index - 1]] }, parent)
 }
 
 function handleSubmit() {
   cardForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const card = createElement('div',{ classList: ['card'] });
-    const left = createElement('div', { classList: ['left'] });
-    const icon = createIcon(typeSelect.value);
-   // const cardType = createElement('span', { classList: ['card-type'], textContent: })
+    const iconOptions = ['fa-burger', 'fa-file-invoice-dollar', 'fa-house', 'fa-suitcase-medical', 'fa-bus', 'fa-film', 'fa-layer-group'];
 
-    cardContainer.append(card);
-    card.append(left, icon)
+
+    const card = createElement('div',{ classList: ['card'] }, cardContainer);
+    const left = createElement('div', { classList: ['left'] }, card);
+    const icon = createIcon(iconOptions, typeSelect.value);
+    const cardType = createElement('span', { classList: ['card-type'], textContent: typeOptions[optionNumber].textContent }, left)
+    const cardAmount = createElement('div', { classList: ['card-amount'], textContent: `$${amountInput.value}` }, card);
+
   })
 
 }
