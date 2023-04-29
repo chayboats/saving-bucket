57146384657093fe7fd750115b655dde2f76c442
Chaley Boatmon <chaleyboatmon@gmail.com>
2023-04-28 20:40:53 -0400

    Set up page differintiation

--- content/content.js
+++ content/content.js
@@ -2,33 +2,48 @@
 // Text - names, headers, options
 // Local storage key
 
+// const title = document.getElementById('expenses-or-income');
+// function determineTitle(link, titleTextContent) {
+//   link.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     title.textContent = titleTextContent;
+//   });
+// }
+
+let incomeLink;
+let expenseLink;
+
+fetch('../nav.html')
+  .then((html) => html.text())
+  .then((data) => {
+    document.getElementById('nav').innerHTML = data;
+    expenseLink = document.getElementById('expense-link');
+    incomeLink = document.getElementById('income-link');
+
+    expenseLink.addEventListener('click', () => setPage('expense'));
+    incomeLink.addEventListener('click', () => setPage('income'));
+  });
+
+function setPage(pageKey) {
+  localStorage.setItem('page', pageKey);
+}
+
+
 const cardForm = document.getElementById('card-form');
 const cards = document.getElementById('cards');
 const amountInput = document.getElementById('amount-input');
 const total = document.getElementById('total');
 const clearAll = document.getElementById('clear-all');
-
-const expenseLink = document.getElementById('expense-link');
-const incomeLink = document.getElementById('income-link');
-const title = document.getElementById('expenses-or-income');
-
 const typeSelect = document.getElementById('type-select');
-const typeOptions = typeSelect?.children || [];
+const typeOptions = typeSelect.children;
+
+let localCards = [];
+let expenseData;
+let incomeData;
+
 const iconOptions = ['fa-burger', 'fa-file-invoice-dollar', 'fa-house', 'fa-suitcase-medical', 'fa-bus', 'fa-film', 'fa-layer-group'];
-let localCards = localStorage.getItem('localCards') ? JSON.parse(localStorage.getItem('localCards')) : [];
+let localExpenseCards = localStorage.getItem('local-expense-cards') ? JSON.parse(localStorage.getItem('local-expense-cards')) : [];
 let trashCans = [];
-
-function determineTitle(link, titleTextContent) {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    title.textContent = titleTextContent;
-  });
-}
-
-function incomePage() {}
-
-determineTitle(expenseLink, 'YOUR EXPENSES');
-determineTitle(incomeLink, 'YOUR INCOME');
 
 function createAndAppendElement(elementType, options = {}, parent) {
   const element = document.createElement(elementType);
@@ -41,31 +56,31 @@ function createAndAppendElement(elementType, options = {}, parent) {
     }
     element[key].add(...options[key]);
   });
-
   parent.append(element);
 
   return element;
 }
 
-function removeElementAndUpdateLocalStorage(element, elements, localElements) {
+function removeElementAndUpdateLocalStorage(localCardsString, element, elements, localCards) {
   const removeIndex = elements.indexOf(element);
 
   elements.splice(removeIndex, 1);
-  updateLocalStorage(() => localElements.splice(removeIndex, 1));
-  updateTotal(localElements);
+  updateLocalStorage(localCardsString, localCards, () => localCards.splice(removeIndex, 1));
+  updateTotal(localCards);
 }
 
-function createDeleteButton(parent) {
+function createDeleteButton(localCardsString, localCards, parent) {
   const trashCan = createAndAppendElement('i', { classList: ['fa-solid', 'fa-trash'] }, parent);
   trashCans.push(trashCan);
 
   trashCan.addEventListener('click', () => {
     parent.remove(trashCan);
-    removeElementAndUpdateLocalStorage(trashCan, trashCans, localCards);
+    removeElementAndUpdateLocalStorage(localCardsString, trashCan, trashCans, localCards);
+    console.log(localCards.length);
   });
 }
 
-function createCard(optionNumber, amount) {
+function createCard(localCardsString, localCards, optionNumber, amount) {
   const card = createAndAppendElement('div', { classList: ['card'] }, cards);
   const cardInfo = createAndAppendElement('div', { classList: ['card-info'] }, card);
   const leftInfo = createAndAppendElement('div', { classList: ['left'] }, cardInfo);
@@ -82,7 +97,7 @@ function createCard(optionNumber, amount) {
   const cardAmountOptions = { classList: ['card-amount'], textContent: formatCurrency(amount) };
   createAndAppendElement('div', cardAmountOptions, cardInfo);
 
-  createDeleteButton(card);
+  createDeleteButton(localCardsString, localCards, card);
 }
 
 function formatCurrency(currency) {
@@ -90,7 +105,7 @@ function formatCurrency(currency) {
   return parseFloat(currency).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 }
 
-function updateTotal() {
+function updateTotal(localCards) {
   let totalAmount = 0;
 
   if (localCards.length > 0) {
@@ -107,23 +122,23 @@ function resetInput(array) {
   });
 }
 
-function updateLocalStorage(callback) {
-  callback();
-  localStorage.setItem('localCards', JSON.stringify(localCards));
-  determineClassForClearAllButton('active', 'inactive');
+function updateLocalStorage(localCardsString, localCards) {
+  localStorage.setItem(localCardsString, JSON.stringify(localCards));
+  determineClassForClearAllButton(localCards, 'active', 'inactive');
 }
 
-function clearAllButton() {
-  if ((clearAll.className = 'active')) {
-    clearAll.addEventListener('click', () => {
+function clearAllButton(localCardsString, localCards) {
+  clearAll.addEventListener('click', () => {
+    if (clearAll.className == 'active') {
       cards.textContent = '';
-      updateLocalStorage(() => (localCards = []));
-      updateTotal();
-    });
-  }
+      localCards = [];
+      updateLocalStorage(localCardsString, localCards);
+      updateTotal(localCards);
+    }
+  });
 }
 
-function determineClassForClearAllButton(class1, class2) {
+function determineClassForClearAllButton(localCards, class1, class2) {
   if (localCards.length > 0) {
     clearAll.className = class1;
     return;
@@ -131,26 +146,40 @@ function determineClassForClearAllButton(class1, class2) {
   clearAll.className = class2;
 }
 
-function handleSubmit() {
+function handleSubmit(localCardsString, localCards) {
   cardForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const inputData = { optionNumber: typeSelect.value, amount: amountInput.value };
-    createCard(inputData.optionNumber, inputData.amount);
-    updateLocalStorage(() => localCards.push(inputData));
-    updateTotal();
+    createCard(localCardsString, localCards, inputData.optionNumber, inputData.amount);
+    updateLocalStorage(localCardsString, localCards, () => localCards.push(inputData));
+    updateTotal(localCards);
+    clearAllButton(localCardsString, localCards);
     resetInput([typeSelect, amountInput]);
+    console.log(localCards.length);
+
     typeSelect.focus();
   });
 }
 
-function restoreData() {
+function restoreData(localCardsString, localCards) {
+  console.log(test);
   localCards.forEach((object) => {
-    createCard(object.optionNumber, object.amount);
+    createCard(localCardsString, localCards, object.optionNumber, object.amount);
   });
   updateTotal(localCards);
-  determineClassForClearAllButton('active', 'inactive');
 }
 
-clearAllButton();
-restoreData();
-handleSubmit();
+// function loadPage(localCardsString, localCards) {
+//   restoreData(localCardsString, localCards);
+//   handleSubmit(localCardsString, localCards);
+//   clearAllButton(localCardsString, localCards);
+//   determineClassForClearAllButton(localCards, 'active', 'inactive');
+// }
+
+//loadPage('local-expense-cards', localExpenseCards);
+
+function onPageLoad() {
+  //Add
+}
+
+clearAllButton('local-expense-cards', localExpenseCards);
--- home/home.js
+++ home/home.js
@@ -1,8 +1,18 @@
-// Read from both keys in local storage and do what you gotta do
-// when tab is clicked on, attach a class - active
+let incomeLink;
+let expenseLink;
 
 fetch('../nav.html')
   .then((html) => html.text())
   .then((data) => {
     document.getElementById('nav').innerHTML = data;
+    expenseLink = document.getElementById('expense-link');
+    incomeLink = document.getElementById('income-link');
+
+    expenseLink.addEventListener('click', () => setPage('expense'));
+    incomeLink.addEventListener('click', () => setPage('income'));
   });
+
+function setPage(pageKey) {
+  localStorage.setItem('page', pageKey);
+  console.log(pageKey);
+}
